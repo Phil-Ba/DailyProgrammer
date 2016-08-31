1f708e581d796775d1a434f710d6bc302ab3968b
philba <p.bayer@freemail.hu>
2016-08-31 23:10:45 +0200

    far more tests for the calculation of the letters because its really hard

--- src/test/scala/at/bayava/rektengular/RektangulatorTest.scala
+++ src/test/scala/at/bayava/rektengular/RektangulatorTest.scala
@@ -62,6 +62,57 @@ class RektangulatorTest extends FunSpec {
 			it("should return (e,k) for the second line") {
 				assert(cut.calculateLettersForOtherLines(2) == ('e', 'k'))
 			}
+			it("should return (k,e) for the third line") {
+				assert(cut.calculateLettersForOtherLines(3) == ('k', 'e'))
+			}
+			it("should return (k,e) for the fifth line") {
+				assert(cut.calculateLettersForOtherLines(5) == ('k', 'e'))
+			}
+		}
+	}
+
+	describe("For the word 'AbcdE'") {
+		val cut = new Rektangulator("AbcdE")
+		describe("the method calculateLettersForOtherLines") {
+			it("should return (b,d) for the second line") {
+				assert(cut.calculateLettersForOtherLines(2) == ('b', 'd'))
+			}
+			it("should return (c,c) for the third line") {
+				assert(cut.calculateLettersForOtherLines(3) == ('c', 'c'))
+			}
+			it("should return (d,b) for the fourth line") {
+				assert(cut.calculateLettersForOtherLines(4) == ('d', 'b'))
+			}
+			it("should return (d,b) for the sixth line") {
+				assert(cut.calculateLettersForOtherLines(6) == ('d', 'b'))
+			}
+			it("should return (c,c) for the seventh line") {
+				assert(cut.calculateLettersForOtherLines(7) == ('c', 'c'))
+			}
+			it("should return (b,d) for the eighth line") {
+				assert(cut.calculateLettersForOtherLines(8) == ('b', 'd'))
+			}
+		}
+	}
+
+	describe("For the word 'AbcdeF'") {
+		val cut = new Rektangulator("AbcdeF")
+		describe("the method calculateLettersForOtherLines") {
+			it("should return (b,e) for the second line") {
+				assert(cut.calculateLettersForOtherLines(2) == ('b', 'e'))
+			}
+			it("should return (c,d) for the third line") {
+				assert(cut.calculateLettersForOtherLines(3) == ('c', 'd'))
+			}
+			it("should return (d,c) for the fourth line") {
+				assert(cut.calculateLettersForOtherLines(4) == ('d', 'c'))
+			}
+			it("should return (e,b) for the fifth line") {
+				assert(cut.calculateLettersForOtherLines(5) == ('e', 'b'))
+			}
+			it("should return (e,b) for the seventh line") {
+				assert(cut.calculateLettersForOtherLines(7) == ('e', 'b'))
+			}
 		}
 	}
 }
